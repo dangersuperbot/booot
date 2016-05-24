eeaabef03ab5339447943b8de680364b63fe516a
dangersuperbot <boomrange.bot@gmail.com>
2016-05-24 18:05:56 +0430

    Update clash.lua

--- plugins/clash.lua
+++ plugins/clash.lua
@@ -28,7 +28,7 @@ if jdat.reason then
      text = text..'\nClan Points: '.. jdat.clanPoints
      text = text..'\nRequired Trophies: '.. jdat.requiredTrophies
      text = text..'\nMembers: '.. jdat.members
-     text = text..'\n\n@GPMod Team'
+     text = text..'\n\n@boomrange team'
      cmd:close()
   return text
  end
@@ -62,7 +62,7 @@ text = leader.."\n"..coleader.."\n\nClan Members:"
   for i = 1, #items do
   text = text..'\n'..i..'- '..items[i].name..'\nlevel: '..items[i].expLevel.."\n"
   end
-  text = text.."\n\n@GPMod"
+  text = text.."\n\n@boomrange"
    cmd:close()
   return text
  end
