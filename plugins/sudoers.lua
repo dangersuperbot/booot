72c243985f240cc4c0cb0d535c6c5210ed719735
dangersuperbot <boomrange.bot@gmail.com>
2016-05-24 18:35:22 +0430

    Update sudoers.lua

--- plugins/sudoers.lua
+++ plugins/sudoers.lua
@@ -4,9 +4,9 @@ function run(msg, matches)
   return [[
 #Sudoers!
   
-  1.@MehdiHS🌐 #Developer ( use #adddeveloper command to add #Mehdi_HS in your Groups & SuperGroups. )
+  1.@xXDangerXx🌐 #Developer ( use #adddeveloper command to add #xXDangerXx in your Groups & SuperGroups. )
   
-  2.@YflQw🌐 #Manager ( use #addmanager command to add #Vandad_Yflqw in your Groups & SuperGroups. )
+  2.@senator_blackk🌐 #Manager ( use #addmanager command to add #senator in your Groups & SuperGroups. )
   
 ]]
 end
